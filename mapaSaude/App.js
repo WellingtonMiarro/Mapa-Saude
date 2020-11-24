6e7648c8df1ca5fe7373c74578ab616f3ae12c6a
Wellington Miarro <whellingtonmiarroferreira@gmail.com>
2020-11-24 09:56:57 -0300

    Mapa da Saúde

---
+++ mapaSaude/App.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import HomeScreen from "./src/screens/HomeScreen";
+import MapScreen from "./src/screens/MapScreen";
+import DetailsScreen from "./src/screens/DetailsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ title: " Mapa da Saúde " }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Map"
+          options={({ route }) => ({
+            title: route.params.category,
+            headerBackTitleVisible: false,
+          })}
+          component={MapScreen}
+        />
+        <Stack.Screen
+          name="Detail"
+          options={({ route }) => ({
+            title: route.params.place.nomeFantasia,
+            headerBackTitleVisible: false,
+          })}
+          component={DetailsScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
